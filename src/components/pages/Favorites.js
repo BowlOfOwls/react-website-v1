dd0fc8fb1a62735fb4a0b183bd16f773eaea3b04
Wei Kang Low <weikang97@gmail.com>
2023-04-21 03:11:14 +0000

    Update 9 files

---
+++ src/components/pages/Favorites.js
@@ -0,0 +1,12 @@
+import React from "react";
+import "../../App.css";
+import Footer from "../Footer";
+
+export default function Favorites() {
+  return (
+    <div>
+      <h1 className="services">Favorites</h1>
+      <Footer />
+    </div>
+  );
+}
