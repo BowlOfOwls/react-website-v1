b810edfae0b973c2bc5462806eb7543ce73d6bfd
Wei Kang Low <weikang97@gmail.com>
2023-04-21 05:48:42 +0000

    Update 5 files

--- src/components/HeroSection.js
+++ src/components/HeroSection.js
@@ -7,16 +7,16 @@ import { ButtonLink } from "./ButtonLink";
 function HeroSection() {
   return (
     <div className="hero-container">
-      <video src="/videos/video-1.mp4" autoPlay loop muted />
-      <h1>ADVENTURE AWAITS</h1>
-      <p>What are you waiting for?</p>
+      <video src="/videos/video-3.mp4" autoPlay loop muted />
+      <h1>WEATHER TRACKER</h1>
+      <p>Find the weather anywhere, Anytime</p>
       <div className="hero-btns">
         <Button
           className="btns"
           buttonStyle="btn--outline"
           buttonSize="btn--large"
         >
-          GET STARTED
+          MY VICINITY
         </Button>
         <ButtonLink
           className="btns"
